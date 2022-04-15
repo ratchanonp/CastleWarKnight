7b69a4e138738d35d0b7db505fa2e93db3808a1e
Ratchanon Panmas <qq.panmas@gmail.com>
2022-04-15 19:37:31 +0700

    Animation System

--- core/src/main/java/com/ratchanon/CastleWarKnight/components/AnimationComponent.java
+++ core/src/main/java/com/ratchanon/CastleWarKnight/components/AnimationComponent.java
@@ -1,4 +1,9 @@
 package com.ratchanon.CastleWarKnight.components;
 
-public class AnimationComponent {
+import com.badlogic.ashley.core.Component;
+import com.badlogic.gdx.graphics.g2d.Animation;
+import com.badlogic.gdx.utils.IntMap;
+
+public class AnimationComponent implements Component {
+    public IntMap<Animation> animations = new IntMap<Animation>();
 }
