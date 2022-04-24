5cb8974c42412dbba8b4a83df239277302e58b52
Ratchanon Panmas <qq.panmas@gmail.com>
2022-04-25 02:41:27 +0700

    :sparkles: Update Point rendering system

--- core/src/main/java/com/ratchanon/CastleWarKnight/systems/PointRenderingSystem.java
+++ core/src/main/java/com/ratchanon/CastleWarKnight/systems/PointRenderingSystem.java
@@ -10,6 +10,8 @@ import com.ratchanon.CastleWarKnight.Asset;
 import com.ratchanon.CastleWarKnight.components.PointComponent;
 import com.ratchanon.CastleWarKnight.components.TransformComponent;
 
+import java.text.DecimalFormat;
+
 public class PointRenderingSystem extends IteratingSystem {
 
     SpriteBatch batch;
@@ -33,7 +35,18 @@ public class PointRenderingSystem extends IteratingSystem {
         batch.begin();
         Asset.font.getData().setScale(0.5f);
         Asset.font.setColor(1, 1, 1, 1);
-        Asset.font.draw(batch, Integer.toString(point.point), pos.pos.x - 45, pos.pos.y + 80, 90, Align.center, true);
+        Asset.font.draw(batch, formatValue(point.point), pos.pos.x - 45, pos.pos.y + 80, 90, Align.center, true);
         batch.end();
+    }
+
+    private String formatValue(float value) {
+        String arr[] = {"", "K", "M", "B", "T", "P", "E"};
+        int index = 0;
+        while ((value / 1000) >= 1) {
+            value = value / 1000;
+            index++;
+        }
+        DecimalFormat decimalFormat = new DecimalFormat("#.##");
+        return String.format("%s %s", decimalFormat.format(value), arr[index]);
     }
 }
