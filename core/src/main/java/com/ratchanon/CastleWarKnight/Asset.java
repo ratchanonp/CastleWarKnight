caba217dbca27293901dc11ba9bbeb150663e591
Ratchanon Panmas <qq.panmas@gmail.com>
2022-04-28 23:47:16 +0700

    :rice_scene: New Asset

--- core/src/main/java/com/ratchanon/CastleWarKnight/Asset.java
+++ core/src/main/java/com/ratchanon/CastleWarKnight/Asset.java
@@ -15,7 +15,11 @@ public class Asset {
     public static TextureAtlas itemsAtlas;
     public static TextureAtlas castleAtlas;
     public static TextureAtlas entityAtlas;
+    public static TextureAtlas entityRunAtlas;
+
+    public static TextureAtlas iconsAtlas;
     public static TextureRegion background;
+    public static Texture newBackground;
     public static TextureRegion logo;
 
     public static TextureRegion gameOverBox;
@@ -23,6 +27,21 @@ public class Asset {
     public static TextureRegion restartHover;
     public static TextureRegion pauseButton;
     public static TextureRegion labelBackground;
+
+    public static TextureRegion iconMute;
+    public static TextureRegion iconSound;
+    public static TextureRegion settings;
+    public static TextureRegion settingsHover;
+    public static TextureRegion start;
+    public static TextureRegion startHover;
+    public static TextureRegion mainMenu;
+    public static TextureRegion mainMenuHover;
+    public static TextureRegion resume;
+    public static TextureRegion resumeHover;
+    public static TextureRegion gamePauseBox;
+    public static TextureRegion gameSettingsBox;
+    public static TextureRegion gameStageCompleteBox;
+    public static TextureRegion gameStageCompleteBoxHover;
 
     // Castle
     public static TextureRegion castleLevel;
@@ -54,6 +73,7 @@ public class Asset {
     public static void load() {
         itemsAtlas = new TextureAtlas(Gdx.files.internal("items.atlas"));
         background = itemsAtlas.findRegion("background");
+        newBackground = new Texture(Gdx.files.internal("demo.png"));
         logo = itemsAtlas.findRegion("Logo");
         gameOverBox = itemsAtlas.findRegion("GameOver");
         restart = itemsAtlas.findRegion("Restart");
@@ -61,40 +81,68 @@ public class Asset {
         pauseButton = itemsAtlas.findRegion("pause");
         labelBackground = itemsAtlas.findRegion("b1_long");
 
+        iconsAtlas = new TextureAtlas(Gdx.files.internal("icons.atlas"));
+        iconMute = iconsAtlas.findRegion("icon_mute");
+        iconSound = iconsAtlas.findRegion("icon_sound");
+        settings = iconsAtlas.findRegion("settings");
+        settingsHover = iconsAtlas.findRegion("settings_clicked");
+        start = iconsAtlas.findRegion("start");
+        startHover = iconsAtlas.findRegion("start_clicked");
+        resume = iconsAtlas.findRegion("resume");
+        resumeHover = iconsAtlas.findRegion("resume_clicked");
+        gamePauseBox = iconsAtlas.findRegion("GamePause");
+        gameSettingsBox = iconsAtlas.findRegion("GameSettings");
+        gameStageCompleteBox = iconsAtlas.findRegion("levelComplete_Stage");
+        gameStageCompleteBoxHover = iconsAtlas.findRegion("levelComplete_Stage_clicked");
+        mainMenu = iconsAtlas.findRegion("mainmenu");
+        mainMenuHover = iconsAtlas.findRegion("mainmenu_clicked");
+
+
         castleAtlas = new TextureAtlas(Gdx.files.internal("castle.atlas"));
         castleLevel = castleAtlas.findRegion("cUnit");
         castleRoof = castleAtlas.findRegion("roof");
 
         entityAtlas = new TextureAtlas(Gdx.files.internal("entities.atlas"));
+        entityRunAtlas = new TextureAtlas(Gdx.files.internal("entitiesRun.atlas"));
         knight = new EntityAnimation(Animator.create(entityAtlas.findRegion("Knight/knightIdle"), 15),
                 Animator.create(entityAtlas.findRegion("Knight/knightAttack"), 22),
-                Animator.create(entityAtlas.findRegion("Knight/knightDeath"), 15));
+                Animator.create(entityAtlas.findRegion("Knight/knightDeath"), 15),
+                Animator.create(entityRunAtlas.findRegion("KnightRun"), 8, 0.125f));
 
         bat = new EntityAnimation(Animator.create(entityAtlas.findRegion("Bat/batIdle"), 8),
                 Animator.create(entityAtlas.findRegion("Bat/batAttack"), 10),
-                Animator.create(entityAtlas.findRegion("Bat/batDeath"), 10));
+                Animator.create(entityAtlas.findRegion("Bat/batDeath"), 10),
+                Animator.create(entityRunAtlas.findRegion("BatRun"), 8, 0.125f));
 
 
         golem = new EntityAnimation(Animator.create(entityAtlas.findRegion("Golem/golemIdle"), 12),
                 Animator.create(entityAtlas.findRegion("Golem/golemAttack"), 16),
-                Animator.create(entityAtlas.findRegion("Golem/golemDeath"), 28));
+                Animator.create(entityAtlas.findRegion("Golem/golemDeath"), 28),
+                Animator.create(entityRunAtlas.findRegion("GolemRun"), 7, 0.125f));
 
 
         witch = new EntityAnimation(Animator.create(entityAtlas.findRegion("Witch/witchIdle"), 7),
                 Animator.create(entityAtlas.findRegion("Witch/witchAttack"), 18),
-                Animator.create(entityAtlas.findRegion("Witch/witchDeath"), 12));
+                Animator.create(entityAtlas.findRegion("Witch/witchDeath"), 12),
+                Animator.create(entityRunAtlas.findRegion("WitchRun"), 8, 0.125f));
 
 
         wolf = new EntityAnimation(Animator.create(entityAtlas.findRegion("Wolf/wolfIdle"), 12),
                 Animator.create(entityAtlas.findRegion("Wolf/wolfAttack"), 16),
-                Animator.create(entityAtlas.findRegion("Wolf/wolfDeath"), 18));
+                Animator.create(entityAtlas.findRegion("Wolf/wolfDeath"), 18),
+                Animator.create(entityRunAtlas.findRegion("WolfRun"), 8, 0.125f));
 
 
         font = new BitmapFont(Gdx.files.internal("font.fnt"), Gdx.files.internal("font.png"), false);
         music = Gdx.audio.newMusic(Gdx.files.internal("song/Song_Skyrim_8-Bit.mp3"));
         music.setLooping(true);
         music.setVolume(0.5f);
-        //music.play();
+
+        if (Settings.musicEnabled) {
+            music.play();
+        } else {
+            music.pause();
+        }
 
         clickSound = Gdx.audio.newSound(Gdx.files.internal("song/Audio/Others/Mouse Click.mp3"));
 
