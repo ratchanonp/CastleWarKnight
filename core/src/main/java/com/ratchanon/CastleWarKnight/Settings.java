0af5c4dd8c82787d6651f7b8c68c3e68b3d4b39c
Ratchanon Panmas <qq.panmas@gmail.com>
2022-04-28 23:48:33 +0700

    Add music settings

--- core/src/main/java/com/ratchanon/CastleWarKnight/Settings.java
+++ core/src/main/java/com/ratchanon/CastleWarKnight/Settings.java
@@ -5,5 +5,16 @@ public class Settings {
     public static final int HEIGHT = 860;
 
     public static boolean soundEnabled = true;
+    public static boolean musicEnabled = false;
+
+    public static void toggleMusic() {
+        musicEnabled = !musicEnabled;
+
+        if (!musicEnabled) {
+            Asset.music.pause();
+        } else {
+            Asset.music.play();
+        }
+    }
 
 }
